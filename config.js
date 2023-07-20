5215e5e0c4478e50d4a2b0311d9cd3c24813a27c
GAJANAYAKANILAME <140082943+GAJANAYAKANILAME@users.noreply.github.com>
2023-07-20 19:58:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94741035064', 'me', true],
+  ['94741035064', 'INDUWARA', true], 
   [''] 
 ] //Number of owners
 
